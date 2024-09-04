c15c724d58e109ca0c92d49ff047262e20f35558
kohei sano <nlee9337@gmail.com>
2024-09-04 22:45:13 +0900

    generate migrate file

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -2,9 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Product> Products { get; set; }
 }
---
+++ Product.cs
@@ -0,0 +1,9 @@
+namespace ProductCatalogApi
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // 他のプロパティを追加...
+    }
+}
